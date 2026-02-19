4f12bbb8e237038e96f0506b49ebc5835022c3a8
thegbars <gbarsotti262@gmail.com>
2026-02-19 13:42:29 -0500

    Mobile navbar fix

--- src/components/shadcn-studio/blocks/navbar-component-01/navbar-component-01.tsx
+++ src/components/shadcn-studio/blocks/navbar-component-01/navbar-component-01.tsx
@@ -16,7 +16,7 @@ type NavigationItem = {
 
 const Navbar = ({ navigationData }: { navigationData: NavigationItem }) => {
   return (
-    <header className='bg-background sticky top-0 z-50'>
+    <header className='bg-whitesticky top-0 z-50'>
       <div className='mx-auto flex max-w-7xl items-center justify-between gap-8 px-4 py-7 sm:px-6'>
         <div className='text-muted-foreground flex flex-1 items-center gap-8 font-medium md:justify-between lg:gap-16'>
             <a href='/'>
@@ -38,7 +38,7 @@ const Navbar = ({ navigationData }: { navigationData: NavigationItem }) => {
             </div>
         </div>
 
-        <div className='flex items-center gap-6'>
+        <div className='flex items-center gap-6 '>
           <DropdownMenu>
             <DropdownMenuTrigger className='md:hidden' asChild>
               <Button variant='outline' size='icon'>
@@ -46,7 +46,7 @@ const Navbar = ({ navigationData }: { navigationData: NavigationItem }) => {
                 <span className='sr-only'>Menu</span>
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className='w-56' align='end'>
+            <DropdownMenuContent className='w-56 bg-white' align='end'>
               <DropdownMenuGroup>
                 {navigationData.map((item, index) => (
                   <DropdownMenuItem key={index}>
