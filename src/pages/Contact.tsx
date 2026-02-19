0b615c289836beeb3737a0bcd190b25d45f0f324
thegbars <gbarsotti262@gmail.com>
2026-02-19 12:04:57 -0500

    Contact updated

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,14 @@
+import Gutters from "@/components/Gutters.tsx";
+
 export default function Contact (){
     return (
         <div>
-
+            <Gutters>
+                <div className="flex flex-col content-center justify-center text-center py-32 gap-4">
+                    <h1 className="text-6xl text-purple-700">Lets work together!</h1>
+                    <p className="text-2xl">Email me at <a href="mailto:gbarsotti262@gmail.com">gbarsotti262@gmail.com</a></p>
+                </div>
+            </Gutters>
         </div>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <Gutters>
             <div className="flex flex-col content-center justify-center text-center py-64 gap-4">
                 <h1 className="text-6xl text-purple-700">Developer. Designer.</h1>
-                <p className="text-2xl">Here is a little blurb about what that could even mean and explaining and something else maybe</p>
+                <p className="text-2xl">Full-stack application designer and developer for web, desktop, and mobile</p>
             </div>
         </Gutters>
 
