4649329637f06fa0a3516db4f65ab8772162ae7e
thegbars <gbarsotti262@gmail.com>
2026-02-24 18:51:19 -0500

    Under construction added to About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
-        <div>
-
+        <div className="flex align-center justify-center py-64">
+            <h1 className="text-purple-700 text-8xl text-center">UNDER CONSTRUCTION</h1>
         </div>
     )
 }
