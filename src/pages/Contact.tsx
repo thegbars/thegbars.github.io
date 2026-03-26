0f2adce99482ee5a1a4171d27361b0aaea281673
thegbars <gbarsotti262@gmail.com>
2026-03-26 12:02:04 -0400

    Contact title spacing fix

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,7 +5,7 @@ export default function Contact (){
     return (
         <div>
             <Gutters>
-                <div className="flex flex-col content-center justify-center text-center pt-32 gap-4">
+                <div className="flex flex-col content-center justify-center text-center mt-16 gap-4">
                     <h1 className="text-6xl text-purple-700">Lets work together!</h1>
                     <p className="text-2xl">Email me at <a className="text-purple-700 hover:underline" href="mailto:gbarsotti262@gmail.com">gbarsotti262@gmail.com</a>
                     </p>
